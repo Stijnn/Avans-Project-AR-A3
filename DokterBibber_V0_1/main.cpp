2b314a65af5e326aa21916d61805185f16e047a2
ChiemStevens <Chiem.stevens@outlook.com>
2020-05-11 14:15:16 +0200

    Added camera image to opengl texture

--- DokterBibber_V0_1/main.cpp
+++ DokterBibber_V0_1/main.cpp
@@ -18,13 +18,23 @@ using tigl::Vertex;
 GLFWwindow* window;
 GLuint textureId = -1;
 
+cv::VideoCapture cap(1);
+cv::Mat frame;
+
 void init();
 void update();
 void draw();
 void cubes();
 
+void initCam();
+void BindCVMat2GLTexture(cv::Mat& image, GLuint& imageTexture);
+void showCameraScreen();
+
+
 int main(void)
 {
+    initCam();
+
     if (!glfwInit())
         throw "Could not initialize glwf";
     window = glfwCreateWindow(1920, 1090, "Dokter Bibber!", NULL, NULL);
@@ -41,6 +51,12 @@ int main(void)
 
 	while (!glfwWindowShouldClose(window))
 	{
+        cap.read(frame);
+        cv::flip(frame, frame, 3);
+        cv::imshow("Dokter Bibber!", frame);
+
+        BindCVMat2GLTexture(frame, textureId);
+
 		update();
 		draw();
 		glfwSwapBuffers(window);
@@ -48,7 +64,6 @@ int main(void)
 	}
 
 	glfwTerminate();
-
 
     return 0;
 }
@@ -72,6 +87,19 @@ void update()
 
 }
 
+void drawTexture()
+{
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+
+    glViewport(0, 0, width, height);
+
+    glClearColor(0.3f, 0.4f, 0.6f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    
+}
+
 void draw()
 {
     int width, height;
@@ -83,7 +111,7 @@ void draw()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     glm::mat4 projection = glm::perspective(55.0f, width / (float)height, 0.1f, 100.0f);
-    glm::mat4 view = glm::lookAt(glm::vec3(0, 10.0f, 0), glm::vec3(0, 0, 0), glm::vec3(0, 1, -1.0f));
+    glm::mat4 view = glm::lookAt(glm::vec3(0, 0, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, -1.0f));
     
 
     tigl::shader->setProjectionMatrix(projection);
@@ -91,22 +119,54 @@ void draw()
     
 
     tigl::shader->enableColor(true);
+    tigl::shader->enableTexture(true);
         
     cubes();
+    showCameraScreen();
+}
+
+void showCameraScreen() {
+    tigl::begin(GL_QUADS);
+
+    tigl::addVertex(Vertex::PT(glm::vec3(-8.5, 0, 0), glm::vec2(0, 0)));
+    tigl::addVertex(Vertex::PT(glm::vec3(-8.5, 4.2, 0), glm::vec2(0, 1)));
+    tigl::addVertex(Vertex::PT(glm::vec3(-4, 4.2, 0), glm::vec2(1, 1)));
+    tigl::addVertex(Vertex::PT(glm::vec3(-4, 0, 0), glm::vec2(1, 0)));
+
+    tigl::end();
 }
 
 void cubes() {  
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(1, 0, 0));
-        model = glm::rotate(model, angle, glm::vec3(1, 1, 0));
 
-        tigl::shader->setModelMatrix(model);
+   
+}
 
-        tigl::begin(GL_QUADS);
-        tigl::addVertex(Vertex::PC(glm::vec3(-5, 1, -5), glm::vec4(-5, 1, 5, 1)));
-        tigl::addVertex(Vertex::PC(glm::vec3(-5, 1, 5), glm::vec4(5, 1, 5, 1)));
-        tigl::addVertex(Vertex::PC(glm::vec3(5, 1, 5), glm::vec4(5, 1, -5, 1)));
-        tigl::addVertex(Vertex::PC(glm::vec3(5, 1, -5), glm::vec4(-5, 1, -5, 1)));
 
-        tigl::end();
+void initCam()
+{
+    // Controle of de camera wordt herkend.
+    if (!cap.isOpened())
+    {
+        std::cout << "Cannot open the video cam" << std::endl;
+        return;
+    }
+}
+
+void BindCVMat2GLTexture(cv::Mat& image, GLuint& imageTexture)
+{
+    glGenTextures(1, &textureId);
+    glBindTexture(GL_TEXTURE_2D, textureId);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D,       
+        0,                 
+        GL_RGB,             
+        image.cols,          
+        image.rows,          
+        0,                   
+        GL_RGB,              
+        GL_UNSIGNED_BYTE,    
+        image.ptr());        
 }
